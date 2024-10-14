628467dde2ed4490d21bc8aa2e32cf583ffa1e23
Wen Qian Zheng <158204243+Wen-Qian-Zheng@users.noreply.github.com>
2024-10-14 14:52:17 -0400

    Update Zheng_Wen_Qian_Lab_5_Part2.cpp

--- Zheng_Wen_Qian_Lab_5_Part2.cpp
+++ Zheng_Wen_Qian_Lab_5_Part2.cpp
@@ -11,21 +11,21 @@ int main()
     double data;
     int quality;
 
-    cout << "How many Gigabytes of data do you have? "; // asks user how many gigabytes of data (prompt)
+    cout << "How many Gigabytes of data do you have?\n> "; // asks user how many gigabytes of data (prompt)
     cin >> data; // takes user input for data variable
-    cout << "Music Quality\n[1] Low\n[2] Normal\n[3] High\n> "; // prompts user to pick between the options
+    cout << "\nMusic Quality\n[1] Low\n[2] Normal\n[3] High\n\n> "; // prompts user to pick between the options
     cin >> quality; // takes user input
 
     switch (quality)
     {
         case 1: // for case 1(input)
-            cout << " > Low              " << fixed << setprecision(2) << (data * 1000) / 43.2 << " Hours" << endl; // calculates for low quality and outputs
+            cout << "\nLow              " << fixed << setprecision(2) << (data * 1000) / 43.2 << " Hours" << endl; // calculates for low quality and outputs
             break;
         case 2: // for case 2(input)
-            cout << " > Normal           " << fixed << setprecision(2) << (data * 1000) / 72 << " Hours" << endl; // calculates for normal quality and outputs
+            cout << "\nNormal           " << fixed << setprecision(2) << (data * 1000) / 72 << " Hours" << endl; // calculates for normal quality and outputs
             break;
         case 3: // for case 3(input)
-            cout << " > High             " << fixed << setprecision(2) << (data * 1000) / 115.2 << " Hours" << endl; // calculates for high quality and outputs
+            cout << "\nHigh             " << fixed << setprecision(2) << (data * 1000) / 115.2 << " Hours" << endl; // calculates for high quality and outputs
             break;
         default: // ""else"""
             cout << "Invalid input." << endl; // if they input anything else just break it!!!!
@@ -34,3 +34,16 @@ int main()
 
     return 0;
 }
+/*
+How many Gigabytes of data do you have?
+> 70
+
+Music Quality
+[1] Low
+[2] Normal
+[3] High
+
+> 3
+
+High             607.64 Hours
+*/
